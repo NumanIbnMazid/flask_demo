ac70747333738b68ba173ccb6bbb96a3f9f19822
Numan Ibn Mazid <numanibnmazid@gmail.com>
2023-02-28 15:40:34 +0600

    Video list and retrieve added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 
 app = Flask(__name__)
@@ -7,3 +8,21 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/videos')
+def list_videos():
+    """
+    Returns all the list of videos inside `videos` folder.
+    """
+    video_dir = os.path.join(os.getcwd(), 'videos')
+    videos = [f for f in os.listdir(video_dir) if os.path.isfile(os.path.join(video_dir, f))]
+    return '<br>'.join(videos)
+
+
+@app.route('/videos/<path:path>')
+def get_video(path):
+    # Access video like: "http://localhost:5000/videos/myvideo.mp4"
+    return send_from_directory('videos', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
